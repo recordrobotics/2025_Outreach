a929491c173ff36dc46b17ce4f3850a108534bf0
Mmohet <jerry20080829@hotmail.com>
2025-08-25 13:36:49 -0400

    Shooter type to kraken

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,30 +1,40 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+// import edu.wpi.first.wpilibj.motorcontrol.Spark;
+// import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import frc.robot.Constants;
 import frc.robot.RobotMap;
 import frc.robot.shuffleboard.ShuffleboardUI;
 
 public class Shooter extends KillableSubsystem {
-  private Talon flywheel =
-      new Talon(RobotMap.Shooter.FLYWHEEL_MOTOR_DEVICE_ID); // old PWM Spark (confusing)
+  private TalonFX flywheel =
+      new TalonFX(RobotMap.Shooter.FLYWHEEL_MOTOR_DEVICE_ID); // old PWM Spark (confusing)
+      private final VoltageOut voltageOut = new VoltageOut(0);
 
-  public Shooter() {
-    toggle(ShooterStates.OFF);
-    ShuffleboardUI.Test.addSlider("Flywheel", flywheel.get(), -1, 1).subscribe(flywheel::set);
-  }
 
-  public enum ShooterStates {
-    SPEAKER,
-    AMP,
-    REVERSE,
-    OFF;
-  }
-
-  public void toggle(double volt) {
-    flywheel.setVoltage(volt);
-  }
+      public Shooter() {
+        toggle(ShooterStates.OFF);
+        // ShuffleboardUI.Test.addSlider("Flywheel", flywheel.get(), -1, 1).subscribe(flywheel::set);
+        ShuffleboardUI.Test
+            .addSlider("Flywheel", 0, -12, 12)
+            .subscribe(volts -> flywheel.setControl(voltageOut.withOutput(volts)));
+      }
+    
+      public enum ShooterStates {
+        SPEAKER,
+        AMP,
+        REVERSE,
+        OFF;
+      }
+    
+      public void toggle(double volts) {
+        // flywheel.set(speed);
+        flywheel.setControl(voltageOut.withOutput(volts));
+    
+      }
 
   public void toggle(ShooterStates state) {
     switch (state) {
@@ -45,3 +55,6 @@ public class Shooter extends KillableSubsystem {
     toggle(ShooterStates.OFF);
   }
 }
+
+
+
