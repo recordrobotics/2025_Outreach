cbc11dde7807c08993dcd1d546befdbd4dc29f97
Isaac <barkerisaacd@gmail.com>
2025-09-06 12:34:31 -0400

    there is no channel, so there is no spin-up time

--- src/main/java/frc/robot/commands/manual/Shoot.java
+++ src/main/java/frc/robot/commands/manual/Shoot.java
@@ -12,9 +12,6 @@ public class Shoot extends SequentialCommandGroup {
 
   private static Shooter _shooter;
 
-  /** Number of seconds it takes for the flywheel to spin up */
-  private final double flywheelSpinupTime = 1.5; // 1.5;
-
   /** Number of seconds it takes to shoot once the flywheel h as been spun up */
 
   /**
@@ -32,7 +29,6 @@ public class Shoot extends SequentialCommandGroup {
     addCommands(
         new InstantCommand(() -> _shooter.toggle(ShooterStates.SPEAKER), _shooter)
             .handleInterrupt(killSpecified),
-        // new WaitCommand(flywheelSpinupTime),
         new WaitCommand(Constants.Shooter.SHOOT_TIME),
         new InstantCommand(() -> _shooter.toggle(ShooterStates.OFF), _shooter)
             .handleInterrupt(killSpecified));
